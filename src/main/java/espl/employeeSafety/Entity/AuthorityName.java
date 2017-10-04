c5f533eb4c6808a97e78fc34990affa6545b01e9
Dakshineshwar Swain <dakshineshwar.swain@eternussolutions.com>
2017-10-04 11:47:30 +0530

    JWT Spring Security Implementation added

---
+++ src/main/java/espl/employeeSafety/Entity/AuthorityName.java
@@ -0,0 +1,5 @@
+package espl.employeeSafety.Entity;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/espl/employeeSafety/Service/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package espl.employeeSafety.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import espl.employeeSafety.Entity.User;
+import espl.employeeSafety.Repository.UserRepository;
+import espl.employeeSafety.jwtSecurity.JwtUserFactory;
+
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
+        } else {
+            return JwtUserFactory.create(user);
+        }
+    }
+}
